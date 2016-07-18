110f8ba05c540fde3b2edbe0b994dadb9c1709f3
Todd Way <tway@vml.com>
2016-07-18 10:23:25 -0500

    updated list.gif to show new animations

--- app/src/main/java/com/vml/listofthings/app/thingdetail/ThingDetailActivity.java
+++ app/src/main/java/com/vml/listofthings/app/thingdetail/ThingDetailActivity.java
@@ -1,6 +1,7 @@
 package com.vml.listofthings.app.thingdetail;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.vml.listofthings.R;
@@ -18,6 +19,8 @@ public class ThingDetailActivity extends BaseActivity implements ThingDetailView
     @Inject ThingDetailPresenter presenter;
     @Bind(R.id.title) TextView titleTextView;
     @Bind(R.id.summary) TextView summaryTextView;
+    @Bind(R.id.content_wrapper)
+    LinearLayout contentWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,9 @@ public class ThingDetailActivity extends BaseActivity implements ThingDetailView
         super.onPostCreate(savedInstanceState);
         App.of(this).component().inject(this);
         presenter.attachView(this, ThingDetailUtil.id(getIntent()));
+        contentWrapper.setAlpha(0);
+        contentWrapper.setScaleX(.8f);
+        contentWrapper.setScaleY(.8f);
     }
 
     @Override
@@ -44,5 +50,6 @@ public class ThingDetailActivity extends BaseActivity implements ThingDetailView
         setToolbarTitle(thingEntity.getTitle());
         titleTextView.setText(thingEntity.getTitle());
         summaryTextView.setText(thingEntity.getSummary());
+        contentWrapper.animate().scaleX(1).scaleY(1).alpha(1).setStartDelay(200).start();
     }
 }
